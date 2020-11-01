180358ddec2c60ccd3129bd99b1385c792d64a29
kmar <k_mar@seznam.cz>
2020-11-01 04:21:37 +0100

    fix canPrune test +4.3

--- cheng4/board.h
+++ cheng4/board.h
@@ -918,12 +918,12 @@ public:
 	inline bool canPrune( Move m ) const
 	{
 		// don't prune passer pushes
-		Square to = MovePack::to( m );
-		Piece p = piece(to);
+		Square from = MovePack::from( m );
+		Piece p = piece(from);
 		if ( PiecePack::type( p ) != ptPawn )
 			return 1;
 		// don't prune passer pushes
-		return (Tables::passerMask[ turn() ][ to ] & pieces( flip(turn()), ptPawn )) != 0;
+		return (Tables::passerMask[ turn() ][ from ] & pieces( flip(turn()), ptPawn )) != 0;
 	}
 
 	// can reduce move?
