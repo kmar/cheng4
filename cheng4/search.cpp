3e9a8cd69108041c5ad07ae3f2c340da709812cf
kmar <k_mar@seznam.cz>
2021-05-14 03:48:21 +0200

    no singular for qs drafts +3.5

--- cheng4/search.cpp
+++ cheng4/search.cpp
@@ -527,7 +527,7 @@ template< bool pv, bool incheck, bool donull >
 	// the problem is not scout search but the extension afterwards (TT pressure?)
 	// the real problem was my replacement scheme though...
 	bool trySingular = useSingular && exclude == mcNone && !isMateScout && depth > 6 && depth+1 < maxDepth &&
-		(lte.u.s.bound & 3) >= btLower && hashmove && !ScorePack::isWin(lte.u.s.score) &&
+		(lte.u.s.bound & 3) >= btLower && hashmove && !ScorePack::isWin(lte.u.s.score) && lte.u.s.depth > 0 &&
 		board.isLegal<incheck, false>(hashmove, board.pins());
 
 	bool doSingular = trySingular &&
