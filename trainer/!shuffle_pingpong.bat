82c39e56d735fdcdb196e56665a35b37e3e665fb
kmar <k_mar@seznam.cz>
2024-03-14 05:52:05 +0100

    fix typo

--- trainer/!shuffle_pingpong.bat
+++ trainer/!shuffle_pingpong.bat
@@ -1,4 +1,4 @@
 echo ping
-shuffler autoplay.bin.bin autoplay_shuffled.bin --min 1000 --max 2000
+shuffler autoplay.bin autoplay_shuffled.bin --min 1000 --max 2000
 echo pong
 shuffler autoplay_shuffled.bin autoplay.bin --min 50000 --max 100000
